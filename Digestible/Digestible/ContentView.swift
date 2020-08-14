24cf4ab6e1c875a27ae36512a7614fb9f3f31b13
Athul Rajagopal <athulrajagopal2002@gmail.com>
2020-08-13 23:09:24 -0600

    XCode File

---
+++ Digestible/Digestible/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Digestible
+//
+//  Created by Athul Rajagopal on 2020-08-13.
+//  Copyright © 2020 DigestLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
