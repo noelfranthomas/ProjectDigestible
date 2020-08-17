30156ad629f0fa32ca1674871e56ccf8925d2130
Athul Rajagopal <athulrajagopal2002@gmail.com>
2020-08-17 17:05:54 -0600

    basic menu added, some extraneous text removed

--- Digestible/Digestible/ContentView.swift
+++ Digestible/Digestible/ContentView.swift
@@ -12,34 +12,43 @@ struct ContentView: View {
     @State private var isTapped = false
     
     var body: some View {
-        ZStack {
-            Rectangle().foregroundColor(Color.gray).edgesIgnoringSafeArea(.all)
-            VStack {
-                Text("DIGESTIBLE")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.orange)
-                Button(action: {
-                    self.isTapped.toggle()
-                }) {
-                    if isTapped {
-                        ArticleContainer()
-                    }
-                    else {
-                        ScrollView {
-                            ImageContainer()
-                        }
-                    }
-                }
-                .buttonStyle(PlainButtonStyle())
-                /*Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec tempor odio...")
-                    .fontWeight(.semibold)
-                    .padding()*/
-                Spacer()
-            }
-        }
-        
+        TabView {
+                           ZStack {
+                               Rectangle().foregroundColor(Color.gray).edgesIgnoringSafeArea(.all)
+                               VStack {
+                                   Text("DIGESTIBLE")
+                                       .font(.largeTitle)
+                                       .fontWeight(.heavy)
+                                       .foregroundColor(Color.orange)
+                                   Button(action: {
+                                       self.isTapped.toggle()
+                                   }) {
+                                       if isTapped {
+                                           ArticleContainer()
+                                       }
+                                       else {
+                                           ScrollView {
+                                               ImageContainer()
+                                           }
+                                       }
+                                   }
+                                   .buttonStyle(PlainButtonStyle())
+                                Spacer()
+                               }
+                           }
+                               .tabItem {
+                                   Image(systemName: "phone.fill")
+                                   Text("First Tab")
+                           }
+                           Text("The content of the second view")
+                               .tabItem {
+                                   Image(systemName: "tv.fill")
+                                   Text("Second Tab")
+                           }
+                       }
+
     }
+    
 }
 
 //The below structure is for previewing and development only
